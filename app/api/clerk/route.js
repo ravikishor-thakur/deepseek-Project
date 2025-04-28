9b96f58e04808d5255f663323e33687099489db5
Ravikishor Thakur <ravikishor022.com>
2025-04-28 22:57:45 +0530

    end to end

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -7,6 +7,16 @@ import { NextRequest } from "next/server";
 export async function POST(req){
     const wh = new Webhook(process.env.SIGNING_SECRET)
     const headerPayload = await headers()
+
+   
+   const existstingUser = await User.findOne({ email: userEmail});
+   if(!existstingUser){
+    await User.create({ email: userEmail, ...otherFields});
+   } else {
+    console.log('User already exists');
+   }
+
+
     const svixHeaders = {
         "svix-id": headerPayload.get("svix-id"),
         "svix-timestamp": headerPayload.get("svix-timestamp"),
@@ -18,7 +28,8 @@ export async function POST(req){
     const payload = await req.json();
     const body =  JSON.stringify(payload);
     const {data, type} = wh.verify(body, svixHeaders)
-
+    
+    console.log(body);
     // Prepare the user data to be saved in the databse
 
     const userData = {
@@ -46,6 +57,7 @@ export async function POST(req){
         default:
             break;
     }
-
-    return NextRequest.json({message: "Event received"});
+     
+    return NextRequest.json({message:  "Event received, Success"});
+   
 }
