0c98cc4c0fe5dace5efda86eaeea8ac338a6d90e
Ravikishor Thakur <ravikishor022@gmail.com>
2025-04-29 20:31:50 +0530

    last end to end

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ const inter = Inter({
 });
 
 export const metadata = {
-  title: "DeepSeek - GreatStack",
+  title: "DeepSeek - Ravikishor",
   description: "Full Stack Project",
 };
 
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -49,7 +49,7 @@ const Sidebar = ({expand, setExpand}) => {
             <Image className={expand ? "w-5" : "w-6.5 mx-auto"} src={expand ? assets.phone_icon : assets.phone_icon_dull} alt=''/>
             <div className={`absolute -top-60 pb-8 ${!expand && "-right-40"} opacity-0 group-hover:opacity-100 hidden group-hover:block transition`}>
                 <div className='relative w-max bg-black text-white text-sm p-3 rounded-lg shadow-lg'>
-                    <Image src={assets.qrcode} alt='' className='w-44'/>
+                    <Image src={assets.new_qr_code} alt='' className='w-44'/>
                     <p>Scan to get DeepSeek App</p>
                     <div className={`w-3 h-3 absolute bg-black rotate-45 ${expand ? "right-1/2" : "left-4"} -bottom-1.5`}></div>
                 </div>
